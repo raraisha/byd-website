921a6559e3b403f9d18f1353693e6cfbc6e68d25
Raisha Afiqah <raishaafiqaah@gmail.com>
2025-09-30 13:13:11 +0700

    first commit

---
+++ supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"
+
+const supabaseUrl = "https://qbixwpmyirbydopzxxwe.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFiaXh3cG15aXJieWRvcHp4eHdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MTIwOTEsImV4cCI6MjA3NDE4ODA5MX0.Yj4B-mI4DVei-abKV96Es6aW2sqX2jWntSdUuwmdsGo"
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
