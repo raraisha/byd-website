bb219578b6d75e60fef29449b1843f486d46f4e4
Aura Anastasya <aura.anastasya08@gmail.com>
2025-11-18 12:07:01 +0700

    update link midtrans

--- api/createTransaction.js
+++ api/createTransaction.js
@@ -61,7 +61,7 @@ export default async function handler(req, res) {
     const auth = Buffer.from(serverKey + ':').toString('base64');
 
     // Call Midtrans
-    const response = await fetch('https://app.midtrans.com/snap/v1/transactions', {
+    const response = await fetch('https://app.sandbox.midtrans.com/snap/v1/transactions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
